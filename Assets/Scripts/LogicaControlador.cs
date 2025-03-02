315b6065323e14236429f10c846b6a5eda3493fb
Alessiiiiii <alexisgor2000@hotmail.com>
2025-03-02 18:07:32 -0300

    Vamoss

--- Assets/Scripts/LogicaControlador.cs
+++ Assets/Scripts/LogicaControlador.cs
@@ -4,15 +4,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class LogicaControlador : MonoBehaviour
 {
     public GameObject panelGane;
     public int numEnemigos;
+    
+   
     // Start is called before the first frame update
     void Start()
     {
-        numEnemigos=GameObject.FindGameObjectsWithTag("Enemigos").Length;
+        numEnemigos=GameObject.FindGameObjectsWithTag("Enemigo").Length;
         panelGane.SetActive(false);
+        
     }
 
     // Update is called once per frame
@@ -23,6 +27,8 @@ public class LogicaControlador : MonoBehaviour
     public void RestarEnemigos()
     {
         numEnemigos--;
+        
+        
         if (numEnemigos <= 0)
 
         {
